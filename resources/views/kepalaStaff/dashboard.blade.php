83d4d83ea467a95dc086d30182f3dbad94dbea61
MAM <khotib.bul@gmail.com>
2023-07-12 09:15:03 +0700

    Dashboard view

--- resources/views/kepalaStaff/dashboard.blade.php
+++ resources/views/kepalaStaff/dashboard.blade.php
@@ -1,183 +1,129 @@
 @extends('layouts.master')
 
 @section('title')
-Dashboard
+    Dashboard
 @endsection
 
 @section('breadcrumb')
-@parent
-<li class="active">Dashboard</li>
+    @parent
+    <li class="active">Dashboard</li>
 @endsection
 
 @section('content')
-<!-- Small boxes (Stat box) -->
-<div class="row">
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-            <div class="inner">
-                {{-- <h3>{{ $kategori }}</h3> --}}
-                <h3>Rp. 2.300.000</h3>
-                <p>Total Penjualan</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-cube"></i>
-            </div>
-            <a href="{{ route('transaksi.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-            <div class="inner">
-                <h3>Rp. 1.500.000</h3>
-
-                <p>Total Belanja</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-cubes"></i>
-            </div>
-            <a href="{{ route('pembelian.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-            <div class="inner">
-                <h3>Rp. 500.000</h3>
-
-                <p>Total Pengeluaran</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-id-card"></i>
-            </div>
-            <a href="{{ route('pengeluaran.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-red">
-            <div class="inner">
-                <h3>Rp. 300.000</h3>
-
-                <p>Profit</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-truck"></i>
-            </div>
-            <a href="{{ route('setengahJadi.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-</div>
-<!-- /.row -->
-<!-- Small boxes (Stat box) -->
-<div class="row">
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-            <div class="inner">
-                <h3>{{ $produk }}</h3>
-
-                <p>Total Inventory</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-cube"></i>
-            </div>
-            <a href="{{ route('LaporanInventory.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-            <div class="inner">
-                <h3>{{ $pengeluaran }}</h3>
-
-                <p>Total Pengeluaran</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-opencart"></i>
-            </div>
-            <a href="{{ route('LaporanPengeluaran.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-            <div class="inner">
-                <h3>{{ $penjualan }}</h3>
-
-                <p>Total Transaksi</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-balance-scale"></i>
-            </div>
-            <a href="{{ route('laporan.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    {{-- <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-red">
-            <div class="inner">
-                <h3>{{ $user }}</h3>
-
-                <p>User</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-id-card"></i>
-            </div>
-            <a href="{{ route('user.index') }}" class="small-box-footer">Lihat <i
-                    class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div> --}}
-    <!-- ./col -->
-</div>
-<!-- /.row (main row) -->
-<div class="row">
-    <div class="col-lg-12">
-        <div class="box">
-            <div class="box-header with-border">
-                <h3 class="box-title">Grafik Pendapatan {{ tanggal_indonesia($tanggal_awal, false) }} s/d
-                    {{ tanggal_indonesia($tanggal_akhir, false) }}</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="chart">
-                            <!-- Sales Chart Canvas -->
-                            <canvas id="salesChart" style="height: 180px;"></canvas>
-                        </div>
-                        <!-- /.chart-responsive -->
-                    </div>
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <?php
+                    $transaksi = DB::table('penjualan')->sum('bayar');
+                    ?>
+                    <h3>{{ $transaksi }}</h3>
+                    <p>Total Penjualan</p>
                 </div>
-                <!-- /.row -->
+                <div class="icon">
+                    <i class="fa fa-dollar"></i>
+                </div>
+                <a href="{{ route('transaksi.index') }}" class="small-box-footer">Lihat <i
+                        class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
-        <!-- /.box -->
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <?php
+                    $pembelian = DB::table('pembelian')->sum('total_harga');
+                    ?>
+                    <h3>{{ $pembelian }}</h3>
+                    <p>Total Belanja</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-shopping-cart"></i>
+                </div>
+                <a href="{{ route('pembelian.index') }}" class="small-box-footer">Lihat <i
+                        class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <?php
+                    $pengeluaran = DB::table('pengeluaran')->sum('nominal');
+                    ?>
+                    <h3>{{ $pengeluaran }}</h3>
+                    <p>Total Pengeluaran</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-cart-plus"></i>
+                </div>
+                <a href="{{ route('pengeluaran.index') }}" class="small-box-footer">Lihat <i
+                        class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <?php
+                    $transaksi = DB::table('penjualan')->sum('bayar');
+                    $pembelian = DB::table('pembelian')->sum('total_harga');
+                    $pengeluaran = DB::table('pengeluaran')->sum('nominal');
+                    $total = $transaksi - ($pembelian + $pengeluaran);
+                    ?>
+                    <h3>{{ $total }}</h3>
+                    <p>Profit</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-line-chart"></i>
+                </div>
+                <a href="{{ route('setengahJadi.index') }}" class="small-box-footer">Lihat <i
+                        class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
     </div>
-    <!-- /.col -->
-</div>
+    <!-- /.row -->
+    <!-- ./col -->
+    </div>
+    <!-- /.row (main row) -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Grafik Pendapatan {{ tanggal_indonesia($tanggal_awal, false) }} s/d
+                        {{ tanggal_indonesia($tanggal_akhir, false) }}</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="chart">
+                                <!-- Sales Chart Canvas -->
+                                <canvas id="salesChart" style="height: 180px;"></canvas>
+                            </div>
+                            <!-- /.chart-responsive -->
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div>
+            </div>
+            <!-- /.box -->
+        </div>
+        <!-- /.col -->
+    </div>
 @endsection
 
 @push('scripts')
-<!-- ChartJS -->
-<script src="{{ asset('AdminLTE-2/bower_components/chart.js/Chart.js') }}"></script>
-<script>
-    $(function() {
+    <!-- ChartJS -->
+    <script src="{{ asset('AdminLTE-2/bower_components/chart.js/Chart.js') }}"></script>
+    <script>
+        $(function() {
             // Get context with jQuery - using jQuery's .get() method.
             var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
             // This will get the first returned node in the jQuery collection.
@@ -204,5 +150,5 @@ Dashboard
 
             salesChart.Line(salesChartData, salesChartOptions);
         });
-</script>
+    </script>
 @endpush
